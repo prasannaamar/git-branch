54cfb4b80af7d1bfe00df425772718c3cf8738ad
prasannaamar <116139914+prasannaamar@users.noreply.github.com>
2026-05-26 11:18:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return "updated Flask sample application on azure hghapp service updated verrsion-7"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 0000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(debug=True, host='0.0.0.0', port=port)
 
